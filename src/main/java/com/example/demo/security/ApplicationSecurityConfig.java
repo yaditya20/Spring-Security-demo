74bb56a2212cb7c7b25797bcd77fd8ddd58d8fcd
Aditya Yadav <yaditya20@gmail.com>
2021-07-25 20:11:48 +0530

    Disabling CSRF protection since we are going to use Postman.

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -12,7 +12,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import static com.example.demo.security.ApplicationUserRole.*;
 
@@ -31,9 +30,11 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                // Enabling CSRF protection for any request that could be processed by a browser by a normal user.
-                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .and()
+//                 To enable CSRF protection for any request that could be processed by a browser by a normal user uncomment the following 2 lines.
+//                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//                .and()
+//                Disabling the CSRF protection again since we are going to use Postman to process any request and avoid CSRF token generation everytime.
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "index.html", "/css/*","/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
